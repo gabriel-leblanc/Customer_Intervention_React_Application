7619fd7f37a79feaa4079b8dfd36e6338506a8ea
Gabriel Leblanc <gabriel.leblanc@icloud.com>
2022-12-13 13:02:32 -0500

    trying to receive api data

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import logo from "./logo.svg";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+    const handleAxios = () => {
+        axios
+            .get("https://java-api.codeboxxtest.xyz/users", {
+                headers: {
+                    token:
+                        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjdXN0b21lcjFAYnVzaW5lc3MuY29tIiwiaXNzIjoiaHR0cHM6Ly9qYXZhLWFwaS5jb2RlYm94eHRlc3QueHl6L2F1dGhlbnRpY2F0ZSJ9.QbJsJ-MZXWieFf_fcAkNWI3S9Skqd-yFVF3S2h-uhfo",
+                },
+            })
+            .then(function(response) {
+                console.log(response);
+                console.log(response.data);
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    };
+
     return (
         <div className="App">
             <header className="App-header">
-                <img
-                    src="../images/rocket-ele-logo.png"
-                    className="App-logo" // fait tourner le logo
-                    alt="logo"
-                />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload. <br /> Just
-                    some random test...
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <input placeholder="Email Address"></input>
-                <input placeholder="password"></input>
-                <button>Log in</button>
+                <img src="img/logos/logo-rocket-elevators.png" alt="logo" />
+                <button onClick={handleAxios}>Call Axios API</button>
             </header>
         </div>
     );
