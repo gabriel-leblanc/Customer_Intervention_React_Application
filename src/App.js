f97f13351656a81f8ad44c7d2bfb27bba3618ca3
Gabriel Leblanc <gabriel.leblanc@icloud.com>
2022-12-13 10:14:59 -0500

    init

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+                <img
+                    src="../images/rocket-ele-logo.png"
+                    className="App-logo" // fait tourner le logo
+                    alt="logo"
+                />
                 <p>
                     Edit <code>src/App.js</code> and save to reload. <br /> Just
                     some random test...
